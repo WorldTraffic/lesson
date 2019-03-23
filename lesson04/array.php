0d65de544a94a0ff0e5ce1ceaf7e8227a770578d
WorldTraffic <worldstraffic@gmail.com>
2019-03-23 17:41:27 +0200

    Update array.php

--- lesson04/array.php
+++ lesson04/array.php
@@ -1,7 +1,7 @@
 <?
 	$arr=array('min'=>rand(1,10),'max'=>rand(10,20));
-	for($x;$x<$arr['max'];$x++){
-		$y=3*pow($x,2)+$x-4;
+	for($arr['min'];$arr['min']<$arr['max'];$arr['min']++){
+		$y=3*pow($arr['min'],2)+$x-4;
 	}
 	echo 'min = '. $arr['min'].'<br>';
 	echo "max = $y ".'<br>';
