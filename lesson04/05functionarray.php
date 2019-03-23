0cfe7868020feb1686ad44b489e932b8129541f2
WorldTraffic <worldstraffic@gmail.com>
2019-03-23 20:30:47 +0200

    ready

--- lesson04/05functionarray.php
+++ lesson04/05functionarray.php
@@ -3,7 +3,6 @@
 	for($arr['min'];$arr['min']<$arr['max'];$arr['min']++){
 		$array[]=3*pow($arr['min'],2)+$arr['min']-4;
 	} 
-	var_dump($array);die();
 	echo 'Минимальное значение функции y='.min($array).'<br>';
 	echo 'Миксимальное значение функции y='.max($array).'<br>';
 ?>
